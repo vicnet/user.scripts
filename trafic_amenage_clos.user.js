5fd10bef762f7fe6cfc3b086f1c36e5cecec4160
vicnet <vo.publique@gmail.com>
2020-12-05 17:14:00 +0100

    add script for trafic amenage web site

---
+++ trafic_amenage_clos.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @id       trafic_amenage_clos
+// @name     Clos forum Trafic
+// @version  1
+// @grant    none
+// @include  http://www.trafic-amenage.com/forum/search.php?search_id=newposts*
+// ==/UserScript==
+
+
+;(function () {
+  var navs = document.querySelectorAll("td span.nav");
+  var nav = navs[navs.length-1];
+  if (nav.innerHTML!=="") {
+  	var parent = nav.parentNode;
+    var br = document.createElement("br");
+   	parent.prepend(br);
+	  nav = document.createElement("span");
+   	parent.prepend(nav);
+  }
+  nav.className="gensmall";
+  
+  var a = document.createElement("a");
+  a.textContent = "Marquer tous les forums comme lus";
+  a.href = "index.php?mark=forums";
+ 	nav.appendChild(a);
+})()
